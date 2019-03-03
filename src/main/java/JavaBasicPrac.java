0e216be5ebc683839603e960700347596295f511
InputYourName <liveintt@gmail.com>
2019-03-03 23:23:04 +0800

    sync

--- src/main/java/JavaBasicPrac.java
+++ src/main/java/JavaBasicPrac.java
@@ -1,27 +1,8 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import classes.Parent;
 
-public class JavaBasicPrac {
+public class JavaBasicPrac{
     public static void main(String[] args) {
-        Demo d1 = new Demo(1);
-        System.out.println(d1.a);
-        swap(d1);
-        System.out.println(d1.a);
-    }
-
-    public static void swap(Demo d1) {
-        d1.a = 23;
-        d1 = new Demo(12);
-    }
-}
-
-class Demo {
-    int a;
-
-    public Demo(int a) {
-        this.a = a;
+        StringBuffer sb = new StringBuffer("abc");
+        Integer a = 1;
     }
 }
--- src/main/java/classes/Child1.java
+++ src/main/java/classes/Child1.java
@@ -3,12 +3,29 @@ package classes;
 import java.io.IOException;
 
 public class Child1 extends Parent {
-    public Child1(int a) {
-        super(a);
+    private String name;
+
+    public Child1() {
+    }
+
+    private Child1(String a, int b, int c) {
+
+    }
+
+    public Child1(String a, char b) {
+
+    }
+
+    public Child1(String a) {
+        this.name = a;
     }
 
     @Override
     public void display() throws IOException {
+        System.out.println(name);
+    }
 
+    public void setName(String name) {
+        this.name = name;
     }
 }
