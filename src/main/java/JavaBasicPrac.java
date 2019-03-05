4ce56c1c32b84e3f8ee3bddae1c0cffa52ec0c72
InputYourName <liveintt@gmail.com>
2019-03-05 09:38:19 +0800

    sync

--- src/main/java/JavaBasicPrac.java
+++ src/main/java/JavaBasicPrac.java
@@ -1,11 +1,38 @@
-import obsever.JDObserver;
-import obsever.ProductList;
+import mybatis.Role;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class JavaBasicPrac {
     public static void main(String[] args) {
-        ProductList observable = ProductList.getInstance();
-        JDObserver observer = new JDObserver();
-        observable.addProductListObserver(observer);
-        observable.addProduct("显示器");
+        SqlSessionFactory sqlSessionFactory = null;
+        String resource = "mybatis-config.xml";
+        InputStream inputStream;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSession sqlSession = null;
+        if (sqlSessionFactory != null) {
+            try {
+                sqlSession = sqlSessionFactory.openSession();
+                Role role = sqlSession.selectOne("mybatis.RoleMapper.selectByName", "dufu");
+                System.out.println(role);
+            } catch (Exception e) {
+                e.printStackTrace();
+                sqlSession.rollback();
+            } finally {
+                if (sqlSession != null) {
+                    sqlSession.close();
+                }
+            }
+        }
+
     }
 }
