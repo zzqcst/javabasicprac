56f1cf89bc8267c6814484ad3ea20dfdcc458c13
InputYourName <liveintt@gmail.com>
2019-03-07 22:13:46 +0800

    sync

--- src/main/java/JavaBasicPrac.java
+++ src/main/java/JavaBasicPrac.java
@@ -11,35 +11,35 @@ import java.util.List;
 
 public class JavaBasicPrac {
     public static void main(String[] args) {
-        SqlSessionFactory sqlSessionFactory = null;
-        String resource = "mybatis-config.xml";
-        InputStream inputStream;
-        try {
-
-            inputStream = Resources.getResourceAsStream(resource);
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        SqlSession sqlSession = null;
-        if (sqlSessionFactory != null) {
-            try {
-                sqlSession = sqlSessionFactory.openSession();
-                RoleMapper mapper = sqlSession.getMapper(RoleMapper.class);
-                List<Role> roles = mapper.selectAll();
-                for (Role role : roles) {
-                    System.out.println(role);
-                }
-                sqlSession.commit();
-            } catch (Exception e) {
-                e.printStackTrace();
-                sqlSession.rollback();
-            } finally {
-                if (sqlSession != null) {
-                    sqlSession.close();
-                }
-            }
-        }
+//        SqlSessionFactory sqlSessionFactory = null;
+//        String resource = "mybatis-config.xml";
+//        InputStream inputStream;
+//        try {
+//
+//            inputStream = Resources.getResourceAsStream(resource);
+//            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        SqlSession sqlSession = null;
+//        if (sqlSessionFactory != null) {
+//            try {
+//                sqlSession = sqlSessionFactory.openSession();
+//                RoleMapper mapper = sqlSession.getMapper(RoleMapper.class);
+//                List<Role> roles = mapper.selectAll();
+//                for (Role role : roles) {
+//                    System.out.println(role);
+//                }
+//                sqlSession.commit();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                sqlSession.rollback();
+//            } finally {
+//                if (sqlSession != null) {
+//                    sqlSession.close();
+//                }
+//            }
+//        }
 
     }
 }
